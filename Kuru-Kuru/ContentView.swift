91ae0285acae5fa0e276be7b0994791ea5b0fec0
Ryan Jin <ryanjin333@gmail.com>
2023-11-20 06:43:45 -0500

    Initial Commit

---
+++ Kuru-Kuru/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kuru-Kuru
+//
+//  Created by Ryan Jin on 2023-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kuru-Kuru/Kuru_KuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kuru_KuruApp.swift
+//  Kuru-Kuru
+//
+//  Created by Ryan Jin on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct Kuru_KuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
